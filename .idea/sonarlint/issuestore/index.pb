f6fb364d1722735bc4bba12aa319c1511316baa8
Hamed <hamedsaeidinia@gmail.com>
2021-03-04 12:25:53 +0330

    Added HelloApiView

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -7,3 +7,11 @@ L
 profiles_project/settings.py,4\6\46b352a78b7eddc1525d2e10ca1196bf625f2735
 W
 'profiles_api/migrations/0001_initial.py,0\a\0a62b78fd35e2fb0a6db4d3ce37832e1795b37c9
+E
+profiles_api/admin.py,9\7\97d03e81814768c4b701867e9e45ec0bf0637589
+H
+profiles_project/urls.py,d\a\da800a01e185ded068c02f8f355ecb6b8265ac8d
+E
+profiles_api/views.py,b\d\bdb932888743c090295b9dc472a7e3b4a2fbf346
+D
+profiles_api/urls.py,f\e\fe0793221b36dd18d2bd11a94b94f74716392349
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    def get(self, request, format=None):
+        """Return a list of api view features"""
+        an_APIView = ['Uses HTTTP method as functions(GET, POST, PUT, PATCH, DELETE)',
+                      'Is similar to the traditional django view',
+                      'Gives you most control over your logic',
+                      'Is mapped manually to URLs']
+
+        return Response({'message': 'Hello World !', 'an_APIView': an_APIView})
